70c22890dc756a5ca70c64e147282dc793fb3ee1
Bophelo <bophelo.mkhwanazi@dariel.co.za>
2018-06-12 16:17:08 +0200

    refactored to include case where there's two numbers, also checked that input does not have more than two numbers

--- src/main/java/com/investec/assessment/StringCalculator.java
+++ src/main/java/com/investec/assessment/StringCalculator.java
@@ -1,12 +1,39 @@
 package com.investec.assessment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
 
 
-    public int Add(String input){
+    public int Add(String input) throws InvalidOutputException {
+        List<String> numbers;
         if(input.isEmpty()){
             return 0;
         }
-        return Integer.parseInt(input);
+        numbers = new ArrayList<>();
+        numbers.addAll(Arrays.asList(input.split(",")));
+
+        if(numbers.size() > 2){
+            throw new InvalidOutputException("Too many numbers in input");
+
+        }
+        return addAllListElements(numbers);
+    }
+
+    private int addAllListElements(List<String> numbers){
+        int output = 0;
+        for(String number: numbers){
+            output += Integer.parseInt(number);
+        }
+        return output;
+    }
+
+    public class InvalidOutputException extends Exception {
+
+        public InvalidOutputException(String message) {
+            super(message);
+        }
     }
 }
--- src/test/java/unit/StringCalculatorUnit.java
+++ src/test/java/unit/StringCalculatorUnit.java
@@ -9,14 +9,33 @@ public class StringCalculatorUnit {
     private StringCalculator stringCalculator = new StringCalculator();
 
     @Test
-    public void testAddForEmptyString(){
+    public void testAddForEmptyString() throws StringCalculator.InvalidOutputException {
         int output = stringCalculator.Add("");
         Assert.assertEquals(output, 0);
     }
 
     @Test
-    public void testAddForSingleNumber(){
+    public void testAddForSingleNumber() throws StringCalculator.InvalidOutputException {
         int output = stringCalculator.Add("11");
         Assert.assertEquals(output, 11);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testAddForNonNumericString() throws StringCalculator.InvalidOutputException {
+        int output = stringCalculator.Add("test string");
+    }
+
+    @Test
+    public void testAddForTwoCommaDelimitedNumbersInAString() throws StringCalculator.InvalidOutputException {
+        int output = stringCalculator.Add("1,2");
+        Assert.assertEquals(3, output);
+
+        output = stringCalculator.Add("1,2,");
+        Assert.assertEquals(3, output);
+    }
+
+    @Test (expected = StringCalculator.InvalidOutputException.class)
+    public void testThatCalculatorAcceptsMaxTwoNumbers() throws StringCalculator.InvalidOutputException {
+        int output = stringCalculator.Add("1,2,3");
     }
 }
